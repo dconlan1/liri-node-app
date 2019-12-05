9e623e4771ec3a58ee482e2d4250aa11befcc0a7
Dave Conlan <daveconlan@me.com>
2019-12-04 17:08:23 -0800

    .

--- key.js
+++ key.js
@@ -4,3 +4,6 @@ exports.spotify = {
     id:process.env.SPOTIFY_ID,
     secret: process.env.SPOTIFY_SECRET
 };
+
+//omdb key
+var queryURL = "http://www.omdbapi.com/?i=tt3896198&apikey=517e907e";
--- liri.js
+++ liri.js
@@ -4,16 +4,71 @@ var keys = require("./keys.js");
 
 var spotify = new Spotify(keys.spotify);
 
-$(document).submit(function () {
-    // prevent default
-    event.preventDefault();
-    // omdb api key
-    var queryURL = "http://www.omdbapi.com/?i=tt3896198&apikey=517e907e";
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function (response) {
+
+var axios = require("axios");
+
+// Grab the movieName which will always be the third node argument.
+var movieName = process.argv[2];
+
+// Then run a request with axios to the OMDB API with the movie specified
+var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=517e907e";
+
+// This line is just to help us debug against the actual URL.
+console.log(queryUrl);
+
+axios.get(queryUrl).then(
+  function(response) {
+    console.log("Release Year: " + response.data.Year);
+    console.log(response);
+  })
+  .catch(function(error) {
+    if (error.response) {
+      // The request was made and the server responded with a status code
+      // that falls out of the range of 2xx
+      console.log("---------------Data---------------");
+      console.log(error.response.data);
+      console.log("---------------Status---------------");
+      console.log(error.response.status);
+      console.log("---------------Status---------------");
+      console.log(error.response.headers);
+    } else if (error.request) {
+      // The request was made but no response was received
+      // `error.request` is an object that comes back with details pertaining to the error that occurred.
+      console.log(error.request);
+    } else {
+      // Something happened in setting up the request that triggered an Error
+      console.log("Error", error.message);
+    }
+    console.log(error.config);
+  });
 
 
+var axios = require("axios");
 
-        var queryURL = ""
+// Then run a request with axios to the OMDB API with the movie specified
+axios.get("http://www.omdbapi.com/?i=tt3896198&apikey=517e907e=remember+the+titans&y=&plot=short&apikey=trilogy").then(
+  function(response) {
+    console.log("The movie's rating is: " + response.data.imdbRating);
+  })
+  .catch(function(error) {
+    if (error.response) {
+      // The request was made and the server responded with a status code
+      // that falls out of the range of 2xx
+      console.log("---------------Data---------------");
+      console.log(error.response.data);
+      console.log("---------------Status---------------");
+      console.log(error.response.status);
+      console.log("---------------Status---------------");
+      console.log(error.response.headers);
+    } else if (error.request) {
+      // The request was made but no response was received
+      // `error.request` is an object that comes back with details pertaining to the error that occurred.
+      console.log(error.request);
+    } else {
+      // Something happened in setting up the request that triggered an Error
+      console.log("Error", error.message);
+    }
+    console.log(error.config);
+  });
+
+  
